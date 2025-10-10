806b2aed9742da7ddc0f16f4648309cd110857c8
XtraCube <72575280+XtraCube@users.noreply.github.com>
2025-10-09 21:25:18 -0400

    update to use managed hook instead of il hook

--- Il2CppInterop.HarmonySupport/Il2CppDetourMethodPatcher.cs
+++ Il2CppInterop.HarmonySupport/Il2CppDetourMethodPatcher.cs
@@ -60,7 +60,7 @@ internal unsafe class Il2CppDetourMethodPatcher : MethodPatcher
     };
 
     private static readonly List<object> DelegateCache = new();
-    private static readonly List<object> ILHookCache = new();
+    private static readonly List<object> HookCache = new();
 
     private INativeMethodInfoStruct modifiedNativeMethodInfo;
 
@@ -152,38 +152,10 @@ internal unsafe class Il2CppDetourMethodPatcher : MethodPatcher
         nativeDetour.Apply();
         modifiedNativeMethodInfo.MethodPointer = nativeDetour.OriginalTrampoline;
 
-        var ilHook = new ILHook(Original, Manipulator);
-        ilHook.Apply();
+        var hook = new Hook(Original, managedHookedMethod);
+        hook.Apply();
 
-        ILHookCache.Add(ilHook);
-
-        void Manipulator(ILContext il)
-        {
-            il.Body.Instructions.Clear();
-            il.Body.ExceptionHandlers.Clear();
-            il.Body.Variables.Clear();
-
-            var c = new ILCursor(il);
-
-            var isStatic = (Original as MethodInfo)?.IsStatic ?? false;
-            var paramCount = Original.GetParameters().Length + (isStatic ? 0 : 1);
-
-            for (var i = 0; i < paramCount; i++)
-            {
-                c.Emit(Mono.Cecil.Cil.OpCodes.Ldarg, i);
-            }
-
-            var target = il.Module.ImportReference(managedHookedMethod);
-
-            var callOp = managedHookedMethod.IsVirtual && !managedHookedMethod.IsStatic
-                ? Mono.Cecil.Cil.OpCodes.Callvirt
-                : Mono.Cecil.Cil.OpCodes.Call;
-
-            c.Emit(callOp, target);
-            c.Emit(Mono.Cecil.Cil.OpCodes.Ret);
-
-            il.Body.MaxStackSize = paramCount + 1;
-        }
+        HookCache.Add(hook);
 
         return managedHookedMethod;
     }
